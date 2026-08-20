f465b78e11b2ce40b8df86649c0a1d77d02816ea
Alihamza980 <ah5204695@gmail.com>
2026-08-21 03:57:21 +0500

    nested loop some practice

---
+++ nested_loop.dart
@@ -0,0 +1,24 @@
+
+import 'dart:io';
+
+void main(){
+
+// for(var i = 1;  i < 5; i++){
+//   for(var j = 1;  j <= 10; j++){
+//     print(i * j);
+//   }
+//   print("=======================");
+  
+// }
+
+// example 2
+
+for(var i = 1;  i < 5; i++){
+  for(var j = 1;  j <= i; j++){
+    stdout.write(j);
+
+  
+  }
+  print("");
+}
+}
