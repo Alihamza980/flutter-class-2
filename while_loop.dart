d8b7787ae6928e519c0a18cdc6b6e478a77fcbd4
Alihamza980 <ah5204695@gmail.com>
2026-08-21 02:57:07 +0500

    while loop some examples

--- while_loop.dart
+++ while_loop.dart
@@ -11,10 +11,35 @@ void main(){
     
   // }
   
-  bool islogin = false;
+//   bool islogin = false;
+//   while(islogin == false){
+//     var email = (stdin.readLineSync()!);
+//     var passward = stdin.readLineSync()!;
+//   if (email == "test@gmail.com" && passward =="123456") {
+//     print("login successful");
+//     islogin = true;
+    
+//   } else {
+//     print("login failed");
+    
+//   }
+// }
+
+// exmaple 3
+var email;
+var passward;
+List <Map> studentsrecord = [
+  {"email" : "test1@gmail.com", "passward": "1245"},
+  {"email" : "test2@gmail.com", "passward": "1345"},
+  {"email" : "test3@gmail.com", "passward": "1235"},
+  {"email" : "test4@gmail.com", "passward": "2345"},
+];
+
+
+bool islogin = false;
   while(islogin == false){
-    var email = (stdin.readLineSync()!);
-    var passward = (stdin.readLineSync()!);
+     email = (stdin.readLineSync()!);
+     passward = (stdin.readLineSync())!;
   if (email == "test@gmail.com" && passward =="123456") {
     print("login successful");
     islogin = true;
@@ -24,4 +49,6 @@ void main(){
     
   }
 }
+print(studentsrecord);
+
 }
