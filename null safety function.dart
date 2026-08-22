5e902eff3ac9aac09ac7594e4f98621c68aee86c
Alihamza980 <ah5204695@gmail.com>
2026-08-23 03:31:10 +0500

    null function

---
+++ null safety function.dart
@@ -0,0 +1,18 @@
+
+// void main(){
+//   studentinfo(name: "bilal" , fblink: "https");
+  
+// }
+// studentinfo({required String name , String? fblink}){
+//   String ui = fblink!;
+//   print(ui);
+// }
+
+// example 2 ternary operator
+
+void main(){
+  int age = 19;
+  String result = age>=18? "allow" : "not allow";
+  print(result);
+
+}
