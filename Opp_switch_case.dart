7b6a88fb62832635023d29970abdd356a29f9150
Alihamza980 <ah5204695@gmail.com>
2026-09-02 03:25:38 +0500

    opp practice

---
+++ Opp_switch_case.dart
@@ -0,0 +1,58 @@
+// void main(){
+
+//   Human obj = Human();
+//   // print(obj.name);
+//   obj.sleep();
+
+
+// }
+
+// class Human{
+//   String name = "Ali hamza";
+//   String age = "23";
+
+//   eat(){
+//     print("$name is eating");
+//   }
+
+//   sleep(){
+//     print("$name is sleeping");
+//   }
+
+//   working(){
+//     print("$name is working");
+//   }
+
+// }
+
+// example 2
+
+
+
+// void main(){
+
+//   num num1 = 4;
+//   num num2 = 7;
+//   var userinput = stdin.readLineSync();
+//   switch (userinput) {
+//     case "+":
+//     print(num1+ num2);
+//       break;
+
+//       case "-":
+//       print(num1 - num2);
+//       break;
+
+//     default:
+//     print("unsupported operation");
+//   }
+// }
+
+// example 3
+
+void main(){
+  print(auth.loginwithfacebookandpassward);
+
+}
+
+  enum auth  {loginwithfacebookandpassward, loginwithgoogle, loginwithinstragram }
